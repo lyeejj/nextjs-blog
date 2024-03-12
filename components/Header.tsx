f47de62193d0392f5dfa9f845cbbaf21fac1d171
lyeejj <lyshine012@gmail.com>
2024-03-12 18:51:59 +0900

    feat: 반응형 메뉴 적용을 위한 Navigation, Header 컴포넌트 변경

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,16 @@
 import Head from "next/head";
 import Navigation from "./Navigation";
 import Link from "next/link";
+import { useState } from "react";
+import { MdMenu, MdClose } from "react-icons/md";
 
 const Header = () => {
+  const [toggleOpen, setToggleOpen] = useState(false);
+
+  const handleToggle = () => {
+    setToggleOpen((prev) => !prev);
+  };
+
   return (
     <>
       <Head>
@@ -11,12 +19,16 @@ const Header = () => {
       <header className="w-full sticky top-0 left-0 z-10 px-5 bg-white h-12 max-w-3xl my-1">
         <div className="w-full h-12 flex items-center justify-between">
           <div className="flex flex-row items-center">
-            <Link href="/" className="font-extralight text-lg">
+            <Link href="/" className="mx-2 font-extralight text-lg">
               YOONLOG
             </Link>
           </div>
           <Navigation />
+          <button onClick={handleToggle} className="md:hidden w-10 relative">
+            {!toggleOpen ? <MdMenu size={24} /> : <MdClose size={24} />}
+          </button>
         </div>
+        {toggleOpen && <Navigation type="toggle" handleToggle={handleToggle} />}
       </header>
     </>
   );
--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,14 +1,30 @@
 import { nav } from "@/data/nav";
 import Link from "next/link";
 
-const Navigation = () => {
+interface INavProps {
+  type?: "toggle" | "navbar";
+  handleToggle?: () => void;
+}
+
+const Navigation = ({ type = "navbar", handleToggle }: INavProps) => {
   return (
-    <nav className="flex items-center gap-4 font-extralight">
+    <nav
+      className={
+        type === "navbar"
+          ? "md:flex hidden items-center gap-4 font-extralight"
+          : "absolute top-12 left-0 w-screen flex flex-col gap-2 px-4 pt-2 pb-4 font-extralight rounded-b-xl bg-gray-100 md:hidden"
+      }
+    >
       {nav.map((item) => (
         <Link
           href={item.location}
           key={item.title}
-          className="hover:font-medium transition-all"
+          className={
+            type === "navbar"
+              ? "hover:font-medium transition-all"
+              : "hover:font-medium transition-all hover:bg-green-300 rounded-md px-2 py-1"
+          }
+          onClick={handleToggle}
         >
           {item.title}
         </Link>
