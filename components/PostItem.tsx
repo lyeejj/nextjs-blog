1a0694180002509aac74ac229ef63b95c843f9b5
lyeejj <lyshine012@gmail.com>
2024-03-12 20:58:44 +0900

    feat: blog페이지 사전렌더링 기능 적용

--- components/PostItem.tsx
+++ components/PostItem.tsx
@@ -1,14 +1,20 @@
 import Link from "next/link";
 
-const PostItem = () => {
+interface IPostItemProps {
+  title: string;
+  description: string;
+  createdAt: string;
+}
+
+const PostItem = ({ createdAt, title, description }: IPostItemProps) => {
   return (
     <Link href="/blog">
       <article className="w-full my-8 hover:-translate-x-1 ">
-        <div className="text-xs text-gray-400">2024.03.05</div>
+        <div className="text-xs text-gray-400">{createdAt}</div>
         <div className="font-bold text-2xl mt-2 hover:text-green-300">
-          ⭐️ 게시물 제목입니다.
+          {title}
         </div>
-        <div className="text-gray-600 text-lg mt-2">게시물 설명입니다.</div>
+        <div className="text-gray-600 text-lg mt-2">{description}</div>
       </article>
     </Link>
   );
--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,13 +1,27 @@
+import { InferGetStaticPropsType } from "next";
+import { allPosts } from "@/.contentlayer/generated";
 import PostItem from "../../components/PostItem";
 
-const Blog = () => {
+const Blog = ({ posts }: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
     <div className="mt-10 flex flex-col">
-      <PostItem />
-      <PostItem />
-      <PostItem />
+      {posts.map((post) => (
+        <PostItem key={post._id} {...post} />
+      ))}
     </div>
   );
 };
+
+export async function getStaticProps() {
+  const posts = allPosts.sort(
+    (a, b) => Number(new Date(b.createdAt)) - Number(new Date(a.createdAt))
+  );
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
 
 export default Blog;
